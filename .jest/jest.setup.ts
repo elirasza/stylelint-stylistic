3b5e1a425ca0e83545dd1b45bdfdb7760c61e358
elirasza <thibaud.laurent@elira.fr>
2023-04-02 23:26:51 +0200

    refactored main codebase to typescript except rules

--- .jest/jest.setup.ts
+++ .jest/jest.setup.ts
@@ -1,6 +1,6 @@
 const { getTestRule } = require('jest-preset-stylelint')
 const failOnConsole = require('jest-fail-on-console')
 
-global.testRule = getTestRule({ plugins: ['./lib/main.js'] })
+global.testRule = getTestRule({ plugins: ['./lib/main.ts'] })
 
 failOnConsole({ shouldFailOnWarn: true })
---
+++ lib/main.ts
@@ -0,0 +1,5 @@
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
+import { Plugin } from 'stylelint'
+
+export default readdirSync(resolve(`${__dirname}/rules`)).map((rule) => require(`./rules/${rule}`).plugin as Plugin);
