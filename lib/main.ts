102647113e752b592f528b5747936880233c0d80
elirasza <thibaud.laurent@elira.fr>
2023-04-03 16:15:18 +0200

    fixed linting rules

--- lib/main.ts
+++ lib/main.ts
@@ -2,4 +2,4 @@ import { resolve } from 'path'
 import { readdirSync } from 'fs'
 import { Plugin } from 'stylelint'
 
-export default readdirSync(resolve(`${__dirname}/rules`)).map((rule) => require(`./rules/${rule}`).plugin as Plugin);
+export default readdirSync(resolve(`${__dirname}/rules`)).map((rule) => require(`./rules/${rule}`).plugin as Plugin)
