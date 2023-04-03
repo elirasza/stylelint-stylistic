633f46fc16d42426c0052fc32d28bc4ad8560531
elirasza <thibaud.laurent@elira.fr>
2023-04-03 18:22:56 +0200

    fixed release action not building

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yarn build
+
 echo $NPM_REGISTRY:_authToken=$NPM_TOKEN >> .npmrc
 npm version $GIT_TAG_NAME --no-git-tag-version
 npm publish --registry https:$NPM_REGISTRY --access public
