d2eb6aa642c8f6ed0252fb970a7cd46ea282791e
elirasza <thibaud.laurent@elira.fr>
2023-04-04 17:49:12 +0200

    removed source maps from built package, added build action

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-yarn build
-
 echo $NPM_REGISTRY:_authToken=$NPM_TOKEN >> .npmrc
 npm version $GIT_TAG_NAME --no-git-tag-version
-npm publish --registry https:$NPM_REGISTRY --access public
+#npm publish --registry https:$NPM_REGISTRY --access public
+npm publish --dry-run
