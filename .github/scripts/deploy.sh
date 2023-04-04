828efe3d6d9ac3424eefe512794b4d6fdf038eba
elirasza <thibaud.laurent@elira.fr>
2023-04-04 17:54:03 +0200

    merged base and quality actions

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,5 +2,4 @@
 
 echo $NPM_REGISTRY:_authToken=$NPM_TOKEN >> .npmrc
 npm version $GIT_TAG_NAME --no-git-tag-version
-#npm publish --registry https:$NPM_REGISTRY --access public
-npm publish --dry-run
+npm publish --registry https:$NPM_REGISTRY --access public
